90ab961164fdada9ed5b2b292c84e657d754317b
VincentWang2961 <vincentwang2961@gmail.com>
2025-02-20 15:54:59 +0800

    Git submodule conflict fixed

--- CS571/hw5/src/components/nav/pages/BadgerBudsAdoptable.jsx
+++ CS571/hw5/src/components/nav/pages/BadgerBudsAdoptable.jsx
@@ -1,6 +1,11 @@
 export default function BadgerBudsAdoptable(props) {
+    console.log(props);
+
     return <div>
         <h1>Available Badger Buds</h1>
         <p>The following cats are looking for a loving home! Could you help?</p>
+
+        {/* TODO: Display all the buddies */}
+
     </div>
 }
